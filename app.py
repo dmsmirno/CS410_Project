f6f27966e611b8a42da43a2ca131fadcd4394f3c
haosen-yao <haoseny2@gmail.com>
2024-12-02 23:37:47 -0500

    draw the plot

--- app.py
+++ app.py
@@ -91,6 +91,24 @@ def analyze_stock(ticker: str) -> dict:
     for key, value in result.items():
         if isinstance(value, np.generic):
             result[key] = value.item()
+
+        # # Generate plot
+    plt.figure(figsize=(12, 6))
+    plt.plot(hist.index, hist["Close"], label="Close Price")
+    plt.plot(hist.index, hist["Close"].rolling(window=50).mean(), label="50-day MA")
+    plt.plot(hist.index, hist["Close"].rolling(window=200).mean(), label="200-day MA")
+    plt.title(f"{ticker} Stock Price (Past Year)")
+    plt.xlabel("Date")
+    plt.ylabel("Price ($)")
+    plt.legend()
+    plt.grid(True)
+
+    # Save plot to file
+    plot_file_path = f"static/stockprice.png"
+    plt.savefig(plot_file_path)
+    print(f"Plot saved as {plot_file_path}")
+    result["plot_file_path"] = plot_file_path
+    
     return result
 
 async def get_Chat_response(text):
@@ -104,20 +122,25 @@ async def get_Chat_response(text):
     )
     report_agent = CodingAssistantAgent(
         name="Report_Agent",
-        model_client=OpenAIChatCompletionClient(model="gpt-4o-mini", api_key="Your Api key"),
+        model_client=OpenAIChatCompletionClient(model="gpt-4o-mini", api_key="Your Api Key"),
         description="Generate a report based on the search and stock analysis results",
         system_message="You are a helpful assistant that can generate a comprehensive report on a given topic based on search and stock analysis. Don't need to add disclaimer When you done with generating the report, reply with TERMINATE.",
     )
     team = RoundRobinGroupChat([stock_analysis_agent, report_agent])
     result = await team.run(text, termination_condition=StopMessageTermination())
     # print(result.messages.TextMessage.content)
+
     ans = ""
     for TextMessage in result.messages:
         # print(type(message))
         ans += TextMessage.content
         ans += '\n'
     # print(ans)
-    return ans
+    graph_url = "static/stockprice.png"
+    return jsonify({
+        "text" : f"{ans}",
+        "graph_url" : f"{graph_url}"
+    })
 
 # @app.route('/get_stock_trend', methods=['POST'])
 # def get_stock_trend():
